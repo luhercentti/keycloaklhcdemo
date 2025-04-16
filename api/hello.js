dd8b62fe53a2f9a6df3711b67d8d48386e9a4036
luhercentti <luishernandezbass@gmail.com>
2025-04-15 20:07:16 -0500

    lhc

--- api/hello.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function (context, req) {
-    context.res = {
-        body: "Hello from Azure Functions!",
-        headers: {
-            'Content-Type': 'text/plain'
-        }
-    };
-};
